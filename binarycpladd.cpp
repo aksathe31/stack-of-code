815295df27c019f84fb0ecc33b81bb0a8a486586
Aditya Sathe <44118736+aksathe31@users.noreply.github.com>
2019-09-09 01:25:21 +0530

    This one has it all (with some glitches)

--- binarycpladd.cpp
+++ binarycpladd.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-struct node
-{
+class node
+{public:
 	int data;
 	node *next,*prev;
 };
@@ -12,109 +12,273 @@ class dll
 node *head1,*tail1;
 	int n;
 
-    //void takelen();
+    void add(dll obj,dll sum);
 	void create();
 	void input();
-	void display();
-	void cpl1(dll obj);
-	void cpl2(dll obj);
+
+	void cpl1(dll);
+	void cpl2(dll,dll,dll);
 
 };
-
-/*void dll::takelen()
+////////////////////////////////////add///////////////////////////////////////////////////////
+void dll::add(dll obj,dll sum)
 {
-cout<<"\n\tenter the length of binary number: ";
-	cin>>n;
-}*/
-
-void dll::cpl2(dll obj)
-{dll compl2;
-    compl2.create();
-	node *p=tail1;
-    node* q=compl2.tail1;
-	int i=0,flag=0;
-cout<<"\n\tTwo's complement is: ";
-	while(p->data!=1)
-	{q->data=p->data;
-	p=p->prev;
-	q=q->prev;
-	i++;
-	flag=1;
+    node *ptr;
+	sum.head1=new node;
+	ptr=sum.head1;
+	node *q,*p;
+	p=q=ptr;
+	int i=1;
+	while(i<n)
+	{
+		p->next=new node;
+		p=p->next;
+		p->prev=q;
+		q=q->next;
+		i++;
 	}
-if(flag==1)
-{
-    q->data=p->data;
-	p=p->prev;
-	q=q->prev;
-	i++;
-}
-while(i<n)
-{//cout<<"("<<p->data<<")";
-	if(p->data==1)
-	q->data=0;
-	else
-	q->data=1;
+	p->next=NULL;
+	sum.tail1=p;
 
-	p=p->prev;
-	q=q->prev;
-	i++;
-}
-node *t=compl2.head1;
-i=0;
+    node*t1,*t2,*s;
+    t1=this->tail1;
+    t2=obj.tail1;
+    s=sum.tail1;
 
-while(i<n)
-{
-	cout<<t->data;
-	t=t->next;
-	i++;
-}
-cout<<"\n\t";
-flag_for_first_time=0;
-}
+    int carry=0;
+ i=n;
+while(i>0)
+  {if((t1->data)==1 && (t2->data)==1)
+    {
+        if(carry==0)
+        {s->data=0;
+         carry=1;
+        }
+        else
+        {s->data=1;
+         carry=1;
+        }
 
-void dll::cpl1(dll obj)
-{
-	node *p=head1;
-	int i=0;
-cout<<"\n\tOne's complement is: ";
-while(i<n)
-{
-	if(p->data==1)
-	cout<<"0";
-	else
-	cout<<"1";
-
-	p=p->next;
-	i++;
-}
-cout<<"\n\t";
-
-}
-
-void dll::display()
-{
-node *p=head1;
-int i=0;
-cout<<"\n\tThe number is: ";
-while(i<n)
-{
-	cout<<p->data;
-	p=p->next;
-	i++;
-}
-cout<<"\n\t";
-
-
-}
-
-void dll::create()
-{if(flag_for_first_time==0)
-    {cout<<"\n\tenter the length of binary number: ";
-	 cin>>n;
     }
+    else if(t1->data==0 && t2->data==0)
+    {
+        s->data=(carry);
+    }
+    else
+    {if(carry==1)
+        {s->data=0;
+         carry=1;
+        }
+     if(carry==0)
+     {s->data=1;
+      carry=0;
+     }
+    }
+    s=s->prev;
+    t1=t1->prev;
+    t2=t2->prev;
+    i--;
+  }
+  s=sum.head1;
+cout<<"\n\tthe number is: ";
+  i=0;
+  while(i<n)
+    {cout<<s->data;
+        s=s->next;
+        i++;
+    }
+cout<<"\n\tCarry="<<carry;
+}
+//////////////////////////////////////////////////////twos complement///////////////////////////////////////////////
+void dll::cpl2(dll compl2,dll compl1,dll csum)
+{node *tobc=this->head1;
+
+
+    node *ptrc;
+
+	compl1.head1=new node;
+	ptrc=compl1.head1;
+	node *qc,*pc;
+	pc=qc=ptrc;
+	int i=1;
+	while(i<n)
+	{
+		pc->next=new node;
+		pc=pc->next;
+		pc->prev=qc;
+		qc=qc->next;
+		i++;
+	}
+	pc->next=NULL;
+	compl1.tail1=pc;
+cout<<"\n\t1's complement is: ";
+i=0;
+while(i<n)
+{
+    if(tobc->data==1)
+    {
+        ptrc->data=0;
+        cout<<ptrc->data;
+        ptrc=ptrc->next;
+    }
+    else
+    {
+        ptrc->data=1;
+        cout<<ptrc->data;
+        ptrc=ptrc->next;
+    }
+    i++;
+    tobc=tobc->next;
+
+}
+
+
+    compl2.head1=new node;
+	node *q,*p;
+	p=q=compl2.head1;
+	p->data=0;
+i=1;
+	while(i<n)
+	{
+		p->next=new node;
+		p->data=0;
+		p=p->next;
+		p->prev=q;
+		q=q->next;
+		i++;
+	}
+	p->data=1;
+	p->next=NULL;
+	compl2.tail1=p;
+cout<<"\n\t2's Complement is ";
+
+    node *sptr;
+	csum.head1=new node;
+	sptr=csum.head1;
+	node *sq,*sp;
+	sp=sq=sptr;
+ i=1;
+	while(i<n)
+	{
+		sp->next=new node;
+		sp=sp->next;
+		sp->prev=sq;
+		sq=sq->next;
+		i++;
+	}
+	sp->next=NULL;
+	csum.tail1=sp;
+
+    node *t1,*t2,*s;
+    t1=compl1.tail1;
+    t2=compl2.tail1;
+    s=csum.tail1;
+
+    int carry=0;
+ i=n;
+while(i>0)
+  {if((t1->data)==1 && (t2->data)==1)
+    {
+        if(carry==0)
+        {s->data=0;
+         carry=1;
+        }
+        else
+        {s->data=1;
+         carry=1;
+        }
+
+    }
+    else if(t1->data==0 && t2->data==0)
+    {
+        s->data=(carry);
+    }
+    else
+    {if(carry==1)
+        {s->data=0;
+         carry=1;
+        }
+     else
+     {s->data=1;
+      carry=0;
+     }
+    }
+    s=s->prev;
+    t1=t1->prev;
+    t2=t2->prev;
+    i--;
+  }
+  s=csum.head1;
+  i=0;
+  while(i<n)
+    {cout<<s->data;
+        s=s->next;
+        i++;
+    }
+cout<<"\n\tCarry="<<carry;
+
+
+
+}
+/////////////////////////////////////////////////ones complement///////////////////////////////
+void dll::cpl1(dll compl1)
+{
+     node *tob=this->head1;
+
+
+    node *ptr;
+
+	compl1.head1=new node;
+	ptr=compl1.head1;
+	node *q,*p;
+	p=q=ptr;
+	int i=1;
+	while(i<n)
+	{
+		p->next=new node;
+		p=p->next;
+		p->prev=q;
+		q=q->next;
+		i++;
+	}
+	p->next=NULL;
+	compl1.tail1=p;
+cout<<"\n\t1's complement is: ";
+i=0;
+while(i<n)
+{
+    if(tob->data==1)
+    {
+        ptr->data=0;
+        cout<<ptr->data;
+        ptr=ptr->next;
+    }
+    else
+    {
+        ptr->data=1;
+        cout<<ptr->data;
+        ptr=ptr->next;
+    }
+    i++;
+    tob=tob->next;
+
+}
+
+
+}
+
+//////////////////////////////////////////////////create/////////////////////////////////////////////////
+void dll::create()
+{   //if(flag_for_first_time==0)
+    cout<<"\n\tenter the length of binary number: ";
+	 cin>>n;
+
+    //else
+      //  n=n;
+
 	node *ptr;
 
-	head1=new node;
+	this->head1=new node;
 	ptr=head1;
 	node *q,*p;
 	p=q=ptr;
@@ -126,17 +290,21 @@ void dll::create()
 		p->prev=q;
 		q=q->next;
 		i++;
-	}tail1=p;
-    flag_for_first_time=1;
+	}
+	p->next=NULL;
+	tail1=p;
+ //   flag_for_first_time=1;
 }
+
+///////////////////////////////////////////////////////input//////////////////////////////////////////////////
 void dll::input()
 {node *ptr;
-	ptr=head1;
+	ptr=this->head1;
 	node *p;
     p=ptr;
 	string s;
 	cout<<"\n\tenter the binary number: ";cin>>s;
-    int i=0;
+int i=0;
 	while(i<n)
 	{	if(s[i]=='1')
 		p->data=1;
@@ -146,17 +314,35 @@ void dll::input()
 
 		i++;
 	}
-
+node *t=head1;
+i=0;
+cout<<"\n\tThe number is: ";
+while(i<n)
+{
+	cout<<t->data;
+	t=t->next;
+i++;
 }
+cout<<"\n\t";
+}
+//////////////////////////////////////////main///////////////////////////////////////////////////////////
 int main()
 {
 
-	dll obj1;
+	dll obj1,obj2,compl1,compl2,sum,csum;
 
 	obj1.create();
 	obj1.input();
-	obj1.display();
-	obj1.cpl1(obj1);
-	obj1.cpl2(obj1);
+
+    //obj1.cpl1(compl1);
+    obj1.cpl2(compl2,compl1,csum);
+flag_for_first_time=0;
+	obj2.create();
+	obj2.input();
+	//obj2.cpl1(obj2);
+	//obj2.cpl2(obj2);
+
+cout<<"\nSum of ";
+	obj1.add(obj2,sum);
 	return 0;
 }
